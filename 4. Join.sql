69c9e51c89e11622b796eaacdb3e0093c95bb255
ryoki_tanaka <ryoki.tanaka@rakus-partners.co.jp>
2021-07-14 14:09:05 +0900

    コードの修正

--- 4. Join.sql
+++ 4. Join.sql
@@ -3,7 +3,7 @@
 select
 r.reserve_id, r.hotel_id, r.customer_id, r.reserve_datetime, r.checkin_date , 
 r.checkin_time, r.checkout_date, r.people_num, r.total_price,
-h.base_price, h.big_area_name, h.small_area_name, h.hotel_latitude, h.hotel_longtitude, h.is_business
+h.base_price, h.big_area_name, h.small_area_name, h.hotel_latitude, h.hotel_longitude, h.is_business
 from
 reserve_tb as r
 join
@@ -17,10 +17,10 @@ and r.people_num = 1; --同上
 with small_area_mst as(
 select 
 small_area_name,
-case when count(hote_id)-1 >= 20
+case when count(hotel_id)-1 >= 20
 then small_area_name else big_area_name end as join_area_id
 from hotel
-group by big_area_name. small_area_name
+group by big_area_name, small_area_name
 )
 
 ,recommend_hotel_mst as (
@@ -37,6 +37,14 @@ from hotel
 
 select
 hotels.hotel_id, r_hotel_mst.rec_hotel_id
-from hotel
+from hotel as hotels
 inner join small_area_mst s_area_mst 
-on hotels
+on hotels.small_area_name = s_area_mst.small_area_name
+inner join recommend_hotel_mst r_hotel_mst
+on s_area_mst.join_area_id = r_hotel_mst.join_area_id
+and hotels.hotel_id != r_hotel_mst.rec_hotel_id;
+
+-- n件前のデータ取得
+
+
+
